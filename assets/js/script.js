547a081caa8be7214eaad15702a598b1a63b5b52
JLatham85 <james.latham85@hotmail.co.uk>
2025-11-24 14:48:07 +0000

    Add main page navigation buttons and style. commentout JS intro until ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Trigger intro modal on page load
-document.addEventListener("DOMContentLoaded", function() {
+/**document.addEventListener("DOMContentLoaded", function() {
   var introModal = new bootstrap.Modal(document.getElementById('introModal'));
   introModal.show();
-});
+});**/
 
